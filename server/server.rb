c1c284c2794a82aee3f194b43c8832d4ee849ec6
Gabriel Vian <g@gabrielvian.com>
2018-12-28 14:05:59 +0100

    refactoring

--- server/server.rb
+++ server/server.rb
@@ -3,27 +3,29 @@ require "securerandom"
 require "json"
 require "fileutils"
 
+load "config.rb"
+
 require_relative "handler.rb"
 require_relative "fulfillment.rb"
 require_relative "debug.rb"
 require_relative "core.rb"
+require_relative "auth.rb"
 
 
-FileUtils.mkdir_p("agents") if !File.exists?("agents")
-
-$config = eval(File.open("config.ini").readlines.join(""))
+File.exists?("agents") ? nil : FileUtils.mkdir_p("agents")
+File.exists?("auth") ? nil : FileUtils.mkdir_p("auth")
 
 server = TCPServer.new $config[:address], $config[:port]
 
 system("cls")
-AP::log("Server bound to #{$config[:address]}:#{$config[:port]}", nil, "server")
+AP.log("Server bound to #{$config[:address]}:#{$config[:port]}", nil, "server")
 $launchdate = Time.new.strftime("%d.%m.%Y-%H.%M.%S")
 
 
 # Tasks on server startup
-agents = AP::getagents(nil, true)
+agents = AP.getagents(nil, true)
 
-AP::log("Running startup tasks...", nil, "server")
+AP.log("Running startup tasks...", nil, "server")
 ranTasks = 0
 agents.each do |agent|
   agent[:startupTasks].each do |task|
@@ -31,28 +33,28 @@ agents.each do |agent|
     OnServerStartup.public_send(task)
   end
 end
-AP::log("Ran #{ranTasks} startup tasks", nil, "server")
+AP.log("Ran #{ranTasks} startup tasks", nil, "server")
 
 
-AP::log("Listening for connections...", nil, "server")
+AP.log("Listening for connections...", nil, "server")
 while true
   begin
     Thread.fork(server.accept) do |socket|
       begin
         id = SecureRandom.hex(2)
-        AP::log("Socket opened", id, "socket")
+        AP.log("Socket opened", id, "socket")
         handler = AP::Handler.new(socket, id)
         handler.run()
       rescue
-        AP::log("Error while creating socket", id, "error")
-        AP::log($!, id, "backtrace")
-        AP::log($!.backtrace, id, "backtrace")
+        AP.log("Error while creating socket", id, "error")
+        AP.log($!, id, "backtrace")
+        AP.log($!.backtrace, id, "backtrace")
         @socket.puts(@headers.merge({"Code"=>"500 Internal Server Error"}).to_json)
       end
-      AP::log("Socket closed", id, "socket")
+      AP.log("Socket closed", id, "socket")
     end
   rescue Interrupt
-    AP::log("Server stopped", nil, "server")
+    AP.log("Server stopped", nil, "server")
     exit!
   end
 end
