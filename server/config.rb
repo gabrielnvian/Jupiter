fd965350094d8499a99ef89638d5a22acb83f377
Gabriel Vian <g@gabrielvian.com>
2018-12-28 14:04:15 +0100

    using rb config with load method

---
+++ server/config.rb
@@ -0,0 +1,7 @@
+$config = {
+  :address=>"localhost",
+  :port=>2556,
+  :BayPath=>"C:/Users/#{ENV["username"]}/Desktop/BAY",
+  :DBpath=>"C:/Users/#{ENV["username"]}/Documents/GitHub/AlphaProtocol/server/agents/files/filebase/database",
+  :rootPWD=>"ciaociao"
+}
