61a0e587fa72dc8ae2c420086b1e651df19151b8
Gabriel Vian <g@gabrielvian.com>
2018-12-30 14:33:29 +0100

    using error codes

--- server/auth.rb
+++ server/auth.rb
@@ -2,16 +2,16 @@ module Auth
   def Auth::adduser(usr, pwd, reqpow, pow = reqpow)
     if reqpow >= pow
       if File.exists?("auth/#{usr}")
-        return false
+        return 1
       else
         FileUtils.mkdir_p("auth/#{usr}")
         File.open("auth/#{usr}/user.ini", "w") do |f1|
           f1.puts "['#{pwd}', #{pow}]"
         end
-        return true
+        return 0
       end
     else
-      return false
+      return 2
     end
   end
 
@@ -20,31 +20,39 @@ module Auth
   end
 
   def Auth.checkpwd(usr, pwd)
-    return eval(File.open("auth/#{usr}/user.ini").readlines.join(""))[0] == pwd
+    return eval(File.open("auth/#{usr}/user.ini").readlines.join(""))[0] == pwd.to_s
   end
 
   def Auth::changepwd(usr, pwd, old = nil, reqpow = 0)
-    pow = AP.getpower(usr)
-    if old = nil
-      if reqpow >= pow
-        File.open("auth/#{usr}/user.ini", "w") do |f1|
-          f1.puts "['#{pwd}', #{pow}]"
-        end
-        return true
-      else
-        return false
-      end
-    else
+    if reqpow > AP.getpower(usr)
       File.open("auth/#{usr}/user.ini", "w") do |f1|
         f1.puts "['#{pwd}', #{pow}]"
       end
-      return true
+      return 0
+    else
+      if Auth.checkpwd(usr, old)
+        File.open("auth/#{usr}/user.ini", "w") do |f1|
+          f1.puts "['#{pwd}', #{pow}]"
+        end
+        return 0
+      else
+        return 1
+      end
     end
   end
 
-  def Auth::deluser(usr)
-    FileUtils.rm_rf("auth/#{usr}")
-    return !File.exists?("auth/#{usr}")
+  def Auth::deluser(usr, pwd = nil, reqpow = 0)
+    if reqpow > AP.getpower(usr)
+      FileUtils.rm_rf("auth/#{usr}")
+      return 0
+    else
+      if Auth.checkpwd(usr, old)
+        FileUtils.rm_rf("auth/#{usr}")
+        return 0
+      else
+        return 1
+      end
+    end
   end
 
 
