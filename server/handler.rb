7db41749a7dc972629d97dce7e9af6a4a1786f3f
Gabriel Vian <g@gabrielvian.com>
2019-01-19 19:35:16 +0100

    fix

--- server/handler.rb
+++ server/handler.rb
@@ -5,7 +5,7 @@ module AP
       @id = id
       @agents = agents
       @headers = {
-        :AP=>$config["version"],
+        :AP=>$config[:version],
         :APS=>nil,
         :Code=>"200 OK",
         :Content=>{}
