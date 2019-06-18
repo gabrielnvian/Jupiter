eefbad11405c8081acb8781df200448158582d9b
Gabriel Vian <g@gabrielvian.com>
2019-06-18 12:17:47 -0400

    remaking filebase

--- client/filebase.rb
+++ client/filebase.rb
@@ -2,131 +2,17 @@ require 'net/ftp'
 
 module FileBase
   def self.addfile(path)
-    if $server
-      if path.nil?
-        path = JClient.input('path').gsub(File::ALT_SEPARATOR, File::SEPARATOR).gsub('"', '')
-      end
-
-      fname = File.basename(path, '.*')
-      fext = File.extname(path)[1..-1]
-      fdate = Time.new.to_i
-
-      fname = nil if fname == ''
-      fext = nil if fext == ''
-      fdate = nil if fdate == ''
-
-      keywords = JClient.input('keywords').split(' ')
-      owner = JClient.input('owner')
-      min_pw = JClient.input('min_pw')
-
-      $server.puts HEADERS.merge(Agent: 'filebase', Cont: { Req: 'UPLOAD', Name: fname, Ext: fext, Date: fdate,
-                                                                    Keywords: keywords, Owner: owner == '' ? nil : owner,
-                                                                    min_pw: min_pw == '' ? nil : min_pw }).to_json
-      response = JClient.jsontosym(JSON.parse($server.gets))
-      if response[:Code] == CODE_OK
-        JClient.output(COLOR::GREEN + response[:Cont][:Resp] + COLOR::CLEAR)
-        ticket = response[:Cont][:Ticket]
-        if FileBase.uploadfile(path, ticket)
-          $server.puts HEADERS.merge(Agent: 'filebase', Cont: { Req: 'SUBMIT', Ticket: ticket }).to_json
-          response = JClient.jsontosym(JSON.parse($server.gets))
-          JClient.output(COLOR::GREEN + response[:Cont][:Resp] + COLOR::CLEAR)
-        end
-      elsif response[:Code] == CODE_ERROR
-        JClient.reset
-        JClient.output(COLOR::RED + response[:Cont][:Resp] + COLOR::CLEAR)
-      else
-        JClient.output(COLOR::RED + response[:Cont][:Resp] + COLOR::CLEAR)
-      end
-    else
-      JClient.output(COLOR::RED + 'Non sei connesso a nessun server' + COLOR::CLEAR)
-      false
-    end
   end
 
   def self.list
-    if $server
-      $server.puts HEADERS.merge(Agent: 'filebase', Cont: { Req: 'LIST' }).to_json
-      response = JClient.jsontosym(JSON.parse($server.gets))
-
-      if response[:Code] == CODE_OK
-        FileBase.show(response, 'Nessun file da elencare')
-      elsif response[:Code] == CODE_ERROR
-        JClient.reset
-        JClient.output(COLOR::RED + response[:Cont][:Resp] + COLOR::CLEAR)
-      else
-        JClient.output(COLOR::RED + response[:Cont][:Resp] + COLOR::CLEAR)
-      end
-    else
-      JClient.output(COLOR::RED + 'Non sei connesso a nessun server' + COLOR::CLEAR)
-      false
-    end
   end
 
   def self.query(type)
-    type = JClient.input('type') if type.nil?
-
-    query = JClient.input('query')
-    query.to_s[0] == '-' ? query = eval(query.to_s[1..-1]) : nil
-
-    if $server
-      $server.puts HEADERS.merge(Agent: 'filebase', Cont: { Req: 'QUERY', Type: type, Query: query }).to_json
-      response = JClient.jsontosym(JSON.parse($server.gets))
-
-      if response[:Code] == CODE_OK
-        FileBase.show(response, 'Nessun file corrisponde alla query')
-      elsif response[:Code] == CODE_ERROR
-        JClient.reset
-        JClient.output(COLOR::RED + response[:Cont][:Resp] + COLOR::CLEAR)
-      else
-        JClient.output(COLOR::RED + response[:Cont][:Resp] + COLOR::CLEAR)
-      end
-    else
-      JClient.output(COLOR::RED + 'Non sei connesso a nessun server' + COLOR::CLEAR)
-      return false
-    end
   end
 
   def self.uploadfile(path, ticket)
-    JClient.output(COLOR::YELLOW+'Caricamento file...'+COLOR::CLEAR)
-    begin
-      ftp = Net::FTP.new
-      ftp.connect($host, '12345')
-      ftp.login('BAYUP', 'bayup')
-      ftp.passive = true
-      ftp.putbinaryfile(path, "#{ticket}#{File.extname(path)}")
-      ftp.close
-      return true
-    rescue Errno::ECONNREFUSED
-      $server.puts HEADERS.merge(Agent: 'filebase', Cont: { Req: 'CANCEL', Ticket: ticket }).to_json
-      JClient.jsontosym(JSON.parse($server.gets))
-      JClient.output(COLOR::RED + 'Impossibile contattare il server...' + COLOR::CLEAR)
-      return false
-    rescue
-      JClient.output(COLOR::RED + "C'e' stato un errore durante il trasferimento del file..." + COLOR::CLEAR)
-      $server.puts HEADERS.merge({ Agent: 'filebase', Cont: { Req: 'CANCEL', Ticket: ticket } }).to_json
-      JClient.jsontosym(JSON.parse($server.gets))
-      return false
-    end
   end
 
   def self.show(resp, msg)
-    resp[:Cont][:Resp].each_index do |i|
-      resp[:Cont][:Resp][i] = JClient.jsontosym(resp[:Cont][:Resp][i])
-    end
-
-    JClient.output(COLOR::YELLOW + msg + COLOR::CLEAR) if resp[:Cont][:Resp].empty?
-
-    files = []
-    resp[:Cont][:Resp].each do |item|
-      files.push([
-                     item[:uid],
-                     item[:name][0..40] + '.' + item[:ext],
-                     Time.at(item[:date]).strftime('%d/%m/%y %H:%M'),
-                     item[:keywords].join(', ')[0..35],
-                     item[:owner]
-                 ])
-    end
-
-    JClient.table(files.unshift(%w[UID Nome Data Parole\ Chiave Proprietario]))
   end
 end
--- server/agents/filebase.rb
+++ server/agents/filebase.rb
@@ -1,311 +1,119 @@
 class Fulfillment
   def filebase(request, userinfo)
-    case request[:Content][:Request]
-    when "UPLOAD"
-      ticket = AP.getsafeid($filebase_tickets.keys)
-      $filebase_tickets[ticket] = {
-        :uid=>AP.getsafeid(FileBase.list(CONFIG[:DBpath]), 6),
-        :name=>request[:Content][:Name],
-        :ext=>request[:Content][:Ext],
-        :date=>request[:Content][:Date],
-        :keywords=>request[:Content][:Keywords],
-        :owner=>request[:Content][:Owner] ? request[:Content][:Owner] : userinfo[0],
-        :minPW=>request[:Content][:minPW] ? request[:Content][:minPW].to_i : userinfo[1]
-      }
-      return {:Content=>{:Response=>"Registrazione eseguita con successo", :Ticket=>ticket}}, true
-    when "SUBMIT"
-      ticket = request[:Content][:Ticket]
-      if $filebase_tickets[ticket] != nil
-        data = $filebase_tickets[ticket]
-        FileUtils.cp("#{CONFIG[:BayUPPath]}/#{ticket}.#{data[:ext]}", "#{CONFIG[:DBpath]}/#{data[:uid]}.#{data[:ext]}")
-        FileUtils.rm("#{CONFIG[:BayUPPath]}/#{ticket}.#{data[:ext]}")
-          
-        data[:size] = File.size("#{CONFIG[:DBpath]}/#{data[:uid]}.#{data[:ext]}")
-
-        FileBase.add(CONFIG[:DBpath], data)
-
-        newhash = {}
-        for key in $filebase_tickets.keys
-          key != ticket ? newhash[key] = $filebase_tickets[key] : nil
-        end
-        $filebase_tickets = newhash
-
-        return {:Content=>{:Response=>"File aggiunto al database"}}, true
-      else
-        return {:Code=>"400 Bad Request", :Content=>{:Response=>"Prima di completare la registrazione e' necessario inviare i dati"}}, true
-      end
-    when "CANCEL"
-      ticket = request[:Content][:Ticket]
-      if $filebase_tickets[ticket] != nil
-        newhash = {}
-        for key in $filebase_tickets.keys
-          key != ticket ? newhash[key] = $filebase_tickets[key] : nil
-        end
-        $filebase_tickets = newhash
-        return {:Content=>{:Response=>"Registrazione annullato"}}, true
-      else
-        return {:Code=>"400 Bad Request", :Content=>{:Response=>"Il ticket di caricamento non esiste"}}, true
-      end
-    when "LIST"
-      user = userinfo[0]
-      power = userinfo[1]
-      reg = FileBase.load(CONFIG[:DBpath])
-      regtosend = []
-      
-      for item in reg
-        if power >= item[:minPW]
-          item2 = {}
-          for key in [:uid, :name, :ext, :date, :keywords, :owner]
-            item2[key] = item[key]
-          end
-          regtosend.push(item2)
-        else
-          if user == item[:owner]
-            item2 = {}
-            for key in [:uid, :name, :ext, :date, :keywords, :owner]
-              item2[key] = item[key]
-            end
-            regtosend.push(item2)
-          end
-        end
-      end
-      return {:Content=>{:Response=>regtosend}}, true
-    when "QUERY"
-      user = userinfo[0]
-      power = userinfo[1]
-      type = request[:Content][:Type]
-      query = request[:Content][:Query]
-
-      case type
-      when "name"
-        result = FileBase::QueryName(query, user, power)
-      when "keywords"
-        result = FileBase::QueryKeywords(query, user, power)
-      when "owner"
-        result = FileBase::QueryOwner(query, user, power)
-      when "before"
-        result = FileBase::QueryBefore(query, user, power)
-      when "after"
-        result = FileBase::QueryAfter(query, user, power)
-      else
-        return {:Code=>"400 Bad Request", :Content=>{:Response=>"Opzione query invalida"}}, true
-      end
-      return {:Content=>{:Response=>result}}, true
+    case request[:Cont][:Req]
+    when 'SUBMIT'
+      # move file
+      # respond
+    when 'CANCEL'
+      # delete file
+      # respond
+    when 'LIST'
+      # list all registered files
+    when 'QUERY'
+      # query through registered files
     end
   end
 end
 
-$filebase_tickets = {}
-
 
 class OnServerStartup
-  def self.filebase_create_subfolders()
-    FileUtils.mkdir_p("#{CONFIG[:DBpath]}/.db")
-    FileUtils.mkdir_p("#{CONFIG[:DBpath]}/.backup")
+  def self.filebase_create_subfolders
+    FileUtils.mkdir_p('agents/files/filebase/db')
   end
 
-  def self.filebase_create_registry()
-    if !File.exist?("#{CONFIG[:DBpath]}/.db/registry")
-      File.open("#{CONFIG[:DBpath]}/.db/registry", "w") do |f1|
-        f1.print ""
-      end
+  def self.filebase_create_registry
+    return true if File.exist?('agents/files/filebase/registry.json')
+
+    File.open('agents/files/filebase/registry.json', 'w') do |f1|
+      f1.print ''
     end
   end
 end
 
 
 module FileBase
-  def FileBase::add(path, item)
-    db = FileBase.load(path)
-    File.open("#{path}/.db/registry", "w") do |f1|
-      f1.puts db.push(item)
-    end
-    return true
+  # Parse registry
+  def self.load(path = 'agents/files/filebase/registry.json')
+    JSON.parse File.readlines(path).join('').chomp
   end
 
-  def FileBase::commit(path, db) # Rewrites the entire registry with the new provided
-    if db.kind_of?(Array)
-      File.open("#{path}/.db/registry", "w") do |f1|
-        f1.puts db
-      end
-      return true
-    else
-      return false
-    end
+  # Validates the item
+  def self.validate(item)
+    item = Jupiter.jsontosym(item)
+    return false if item.keys.include? :uid
+    return false if item[:uid].length != 10
+    return false if item.keys.include? :name
+    return false if item[:name].nil?
+    return false if item.keys.include? :ext
+    return false if item[:ext].nil?
+    return false if item.keys.include? :owner
+    return false if item[:owner].nil?
+    return false if item.keys.include? :kwords
+    return false unless item[:kwords].is_a?(Array)
   end
 
-  def FileBase::load(path) # Loads and returns the registry
-    if File.exist?("#{path}/.db/registry")
-      db = []
-      for entry in File.readlines("#{path}/.db/registry")
-        db.push(eval(entry))
-      end
-      return db
-    else
-      return []
-    end
+  # Add file to registry
+  def self.add(item, path = 'agents/files/filebase/registry.json')
+    reg = FileBase.load(path)
+    return false unless FileBase.validate(item)
+
+    reg.push(item)
   end
 
-  def FileBase::get(path, uid)
-    db = FileBase.load(path)
-    for entry in db
-      return entry if entry[:uid] == uid
+  # Delete file from registry
+  def self.del(path = 'agents/files/filebase/registry.json', uid)
+    reg = FileBase.load(path)
+    reg.each_with_index do |entry, i|
+      reg.delete_at(i) if entry[:uid] == uid
     end
-    return false
+    reg
   end
 
-  def FileBase::check(path) # Deletes non existing files
-    db = FileBase.load(path)
-    db.each_with_index do |entry, i|
-      File.exist?("#{path}/#{entry}") ? nil : db.delete_at(i)
+  # Generates a new registry from the files found
+  def self.regen(path = 'agents/files/filebase/registry.json')
+    reg = []
+    uids = FileBase.get_uids(path) || []
+    Dir.entries(path)[2..-1].each do |entry|
+      h = {
+          uid: Jupiter.getsafeid(uids, 10),
+          name: entry.split('.'[0..-2].join('.')),
+          ext: entry.split('.')[-1],
+          owner: nil,
+          kwords: ['unknown']
+      }
+      reg.push(h)
     end
-    FileBase.commit(path, db)
-    return true
+    reg
   end
 
-  def FileBase::list(path)
-    list = []
-    for entry in FileBase.load(path)
-      list.push(entry[:uid])
+  # Returns an array containing all uids
+  def self.get_uids(path = 'agents/files/filebase/registry.json')
+    return false unless File.exist?(path)
+
+    uids = []
+    FileBase.load(path).each do |entry|
+      uids.push(entry[:uid])
     end
-    return list
+    uids
   end
 
-  def FileBase::QueryName(query, user, power)
-    db = FileBase.load(CONFIG[:DBpath])
-    result = []
-    for item in db
-      if power >= item[:minPW]
-        if item[:name].downcase().include?(query.downcase())
-          item2 = {}
-          for key in [:uid, :name, :ext, :date, :keywords, :owner]
-            item2[key] = item[key]
-          end
-          result.push(item2)
-        end
-      else
-        if user == item[:owner]
-          if item[:name].downcase().include?(query.downcase())
-            item2 = {}
-            for key in [:uid, :name, :ext, :date, :keywords, :owner]
-              item2[key] = item[key]
-            end
-            result.push(item2)
-          end
-        end
-      end
-    end
-    return result
+  # List all files in registry
+  def self.list(path)
   end
 
-  def FileBase::QueryKeywords(query, user, power)
-    db = FileBase.load(CONFIG[:DBpath])
-    result = []
-    for item in db
-      if power >= item[:minPW]
-        if item[:keywords].include?(query)
-          item2 = {}
-          for key in [:uid, :name, :ext, :date, :keywords, :owner]
-            item2[key] = item[key]
-          end
-          result.push(item2)
-        end
-      else
-        if user == item[:owner]
-          if item[:keywords].include?(query)
-            item2 = {}
-            for key in [:uid, :name, :ext, :date, :keywords, :owner]
-              item2[key] = item[key]
-            end
-            result.push(item2)
-          end
-        end
-      end
-    end
-    return result
+  # Query by ...
+  def self.query_name(query, user, power)
   end
 
-  def FileBase::QueryOwner(query, user, power)
-    db = FileBase.load(CONFIG[:DBpath])
-    result = []
-    query.nil? ? query = user : nil
-    for item in db
-      if power >= item[:minPW]
-        if item[:owner] == query
-          item2 = {}
-          for key in [:uid, :name, :ext, :date, :keywords, :owner]
-            item2[key] = item[key]
-          end
-          result.push(item2)
-        end
-      else
-        if user == item[:owner]
-          if item[:owner] == query
-            item2 = {}
-            for key in [:uid, :name, :ext, :date, :keywords, :owner]
-              item2[key] = item[key]
-            end
-            result.push(item2)
-          end
-        end
-      end
-    end
-    return result
+  def self.query_keywords(query, user, power)
   end
 
-  def FileBase::QueryBefore(query, user, power)
-    db = FileBase.load(CONFIG[:DBpath])
-    result = []
-    query.nil? ? query = Time.new.to_i : nil
-    for item in db
-      if power >= item[:minPW]
-        if item[:date] < query.to_i
-          item2 = {}
-          for key in [:uid, :name, :ext, :date, :keywords, :owner]
-            item2[key] = item[key]
-          end
-          result.push(item2)
-        end
-      else
-        if user == item[:owner]
-          if item[:date] < query.to_i
-            item2 = {}
-            for key in [:uid, :name, :ext, :date, :keywords, :owner]
-              item2[key] = item[key]
-            end
-            result.push(item2)
-          end
-        end
-      end
-    end
-    return result
+  def self.query_owner(query, user, power)
   end
 
-  def FileBase::QueryAfter(query, user, power)
-    db = FileBase.load(CONFIG[:DBpath])
-    result = []
-    query.nil? ? query = Time.new.to_i : nil
-    for item in db
-      if power >= item[:minPW]
-        if item[:date] > query.to_i
-          item2 = {}
-          for key in [:uid, :name, :ext, :date, :keywords, :owner]
-            item2[key] = item[key]
-          end
-          result.push(item2)
-        end
-      else
-        if user == item[:owner]
-          if item[:date] > query.to_i
-            item2 = {}
-            for key in [:uid, :name, :ext, :date, :keywords, :owner]
-              item2[key] = item[key]
-            end
-            result.push(item2)
-          end
-        end
-      end
-    end
-    return result
+  def self.query_before(query, user, power)
+  end
+
+  def self.query_after(query, user, power)
   end
 end
