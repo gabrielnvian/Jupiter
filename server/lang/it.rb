a225c1b7adf8313822651c8ed9fa30cd72af1903
Gabriel Vian <g@gabrielvian.com>
2019-06-18 21:29:12 -0400

    added filebase

--- server/lang/it.rb
+++ server/lang/it.rb
@@ -1,13 +1,14 @@
 module LANG
+  # Main
   SERVER_START_FORCED = "L'avvio del server e' stato forzato"
   DEBUG_MODE_ON = "Avvio del server in modalita' di debug"
   FOLDER_ALREADY_IN_USE = "Un'altra istanza e' in esecuzione utilizzando la stessa cartella (.running)"
   ADMIN_REQUIRED = 'Sono richiesti privilegi di amministratore per avviare il server'
-  SERVER_LISTENING = "Server in ascolto su !1!:!2!"
+  SERVER_LISTENING = 'Server in ascolto su !1!:!2!'
   LAUNCH_START_SCRIPTS = 'Lancio script di avvio in corso...'
-  LAUNCHED_SCRIPTS = "Lanciati !1! script di avvio"
+  LAUNCHED_SCRIPTS = 'Lanciati !1! script di avvio'
   WAITING_CONNECTIONS = 'In attesa di connessioni...'
-  NEW_CONNECTION = "Nuova connessione"
+  NEW_CONNECTION = 'Nuova connessione'
   ERROR_CREATE_SOCKET = 'Errore nel creare il socket'
   CODE500 = 'Errore interno del server'
   CLOSED_CONNECTION = 'Connessione chiusa'
@@ -23,5 +24,12 @@ module LANG
   LAUNCH_AGENT = "Lancio di '!1!' in corso"
   RESPONDING_REQUEST = 'Responding to request...'
   PW_LVL_NEEDED = "E' necessario un livello PW!1! (PW!2!)"
-  AUTHORIZATION_ERROR = "Errore di autorizzazione"
+  AUTHORIZATION_ERROR = 'Errore di autorizzazione'
+  AGENT_LANG_NOT_DEFINED = "Traduzione !2! non trovata per l'agente !1!"
+  AGENT_LANG_NOT_DEFINED_HOWTO = "Aggiungi le traduzioni dal file di configurazione dell'agente"
+
+  # FileBase
+  FILEBASE_FILE_ADDED = 'File archiviato con successo'
+  FILEBASE_NO_FILE_TO_SHOW = 'Nessun file da elencare'
+  FILEBASE_FILE_LIST = "Elenco file per l'utente !1!"
 end
